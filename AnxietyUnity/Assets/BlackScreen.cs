fbb591d3713507562b53e30c9c1843f9c10bb267
Edudjr <eduardo.djr@hotmail.com>
2015-03-08 17:26:24 -0300

    Editor, Gizmos, Heightmaps

---
+++ AnxietyUnity/Assets/BlackScreen.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlackScreen : MonoBehaviour {
+	public GameObject blackScreen;
+	public GameObject countdownText;
+	public float countdownTime = 7.0f;
+
+	// Use this for initialization
+	void OnEnable(){
+		OVRManager.HSWDismissed += FadeOut;
+	}
+
+	void OnDisable(){
+		OVRManager.HSWDismissed -= FadeOut;
+	}
+
+	void FadeOut(){
+		StartCoroutine (FadeOutHelper());
+	}
+
+	IEnumerator FadeOutHelper(){
+		Color color = blackScreen.renderer.material.color;
+		while (color.a > 0.0f) {
+			color.a -= 0.05f;
+			blackScreen.renderer.material.color = color;
+			yield return null;
+		}
+
+	}
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		countdownTime -= Time.deltaTime;
+
+		if (countdownTime < 0) {
+			FadeOut ();
+			countdownTime = 0;
+			TextMesh t = (TextMesh)countdownText.GetComponent (typeof(TextMesh));
+			t.text = "";
+		} else if (countdownTime > 0) {
+			TextMesh t = (TextMesh)countdownText.GetComponent (typeof(TextMesh));
+			t.text = countdownTime.ToString ("F0");
+		}
+	}
+}
---
+++ AnxietyUnity/Assets/WaterMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterMovement : MonoBehaviour {
+	public float minHeight = 0.0f;
+	public float maxHeight = 2.0f;
+	public float speed = 1.0f;
+
+	public bool goingUp;
+
+	// Use this for initialization
+	void Start () {
+		goingUp = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (goingUp) {
+			transform.Translate (Vector3.up * speed);
+		} else {
+			transform.Translate(Vector3.up * -speed);
+		}
+
+		if (transform.position.y > maxHeight) {
+			goingUp = false;
+		} else if (transform.position.y < minHeight) {
+			goingUp = true;
+		}
+	}
+
+}
